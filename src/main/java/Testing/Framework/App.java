b3177f0f21ca51d48532bd7ace697773f29077b7
Vimlesh Kumar <vimlesh073@gmail.com>
2019-08-26 22:16:04 +0530

    code change

--- src/main/java/Testing/Framework/App.java
+++ src/main/java/Testing/Framework/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        int a,b,c;
+        a =44;
+        b =554;
+        
+        c =a+b;
+        System.out.println(c);
+        
     }
 }
